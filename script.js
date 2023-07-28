b02ea97cd82a60b90c0648692ac21182c6b9c680
Artemus Hayden <grayson2005hayden@gmail.com>
2023-07-27 20:11:26 -0500

    Merge functions and Fix logic errors

--- script.js
+++ script.js
@@ -6,70 +6,62 @@ const slideDisplay = document.querySelector('.slider-value');
 userInput.oninput = (() => {
     let value = userInput.value
     slideDisplay.textContent = value 
-    createGrid(value)
+    let colorSelector = document.querySelector('.color-selector');
+    createGrid(value, colorSelector)
 });
 
-function createGrid (userInput){
+function createGrid (userGrid, colorSelector){
     const sketchCont = document.querySelector(".sketch-cont");
-    let gridAmount = (userInput * userInput);
+    let gridAmount = (userGrid * userGrid);
     sketchCont.innerHTML = '';
     for (i = 0; i < gridAmount; i++) {
         let grid = document.createElement('div');
-        grid.style.height = (600 / userInput) + 'px';
-        grid.style.width = (600 / userInput) + 'px';
+        grid.style.height = (600 / userGrid) + 'px';
+        grid.style.width = (600 / userGrid) + 'px';
         grid.classList.add('grid');
         sketchCont.appendChild(grid);
-    }
 
-}
-// ^ creates grid
-
-
-const allGrids = document.querySelectorAll('.grid');
-let colorSelector = document.querySelector('.color-selector');
-const clear = document.querySelector('.clear');
-const eraser = document.querySelector('.eraser');
-let isMousedown = false;
-let eraserMode = false;
-
-allGrids.forEach(grid => {
-
-    colorSelector.addEventListener ('click', () =>{
-        eraserMode = false;
-    });
-
-    eraser.addEventListener('click', () =>{
-        eraserMode = true;
-    });
-
-    grid.addEventListener("mousedown", () => {
-        isMousedown = true;
-        if (eraserMode === true) {
-            grid.style.backgroundColor = defaultColor;
-        } else{
-            grid.style.backgroundColor = colorSelector.value;
-        }
-    });
-
-    grid.addEventListener("mouseup", () => {
-        isMousedown = false;
-    });
-
-    grid.addEventListener("mousemove", () => {
-        if (isMousedown) {
+        grid.addEventListener("mousedown", () => {
+            isMousedown = true;
             if (eraserMode === true) {
                 grid.style.backgroundColor = defaultColor;
             } else{
                 grid.style.backgroundColor = colorSelector.value;
             }
-        } else {
-            return;
+        });
+    
+        grid.addEventListener("mouseup", () => {
+            isMousedown = false;
+        });
+    
+        grid.addEventListener("mousemove", () => {
+            if (isMousedown) {
+                if (eraserMode === true) {
+                    grid.style.backgroundColor = defaultColor;
+                } else{
+                    grid.style.backgroundColor = colorSelector.value;
+                }
+            } else {
+                return;
+        }
+        });
+
+        const clear = document.querySelector('.clear');
+        const eraser = document.querySelector('.eraser');
+        let eraserMode = false;
+
+        colorSelector.addEventListener ('click', () =>{
+            eraserMode = false;
+        });
+    
+        eraser.addEventListener('click', () =>{
+            eraserMode = true;
+        });
+
+        clear.addEventListener('click', () =>{
+            grid.style.backgroundColor = defaultColor;
+        });
     }
-    });
 
-    clear.addEventListener('click', () =>{
-        grid.style.backgroundColor = defaultColor;
-    });
-});
-
-// ^ allows grid to be colored when selected 
+}
+// ^ creates grid and adds conditions based on which buttons are pressed
